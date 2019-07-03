24a64aa4258b13e41c13261da57f0965b01e1cde
GiuliaGallorini <giuliagallorini7@gmail.com>
2019-07-03 15:05:42 +0100

    Corrected

---
+++ models/Website.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const websiteSchema = new Schema({
+  name: String, 
+  url: String, 
+  nbOfVotes: { type: Number, default: 0 }
+});
+
+const Website = mongoose.model("Website", websiteSchema);
+module.exports = Website;
+
+
+// => Define a "websites" collection
